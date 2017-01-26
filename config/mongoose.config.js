3d4fbea6210e4a9bdd73323592a223ccd74e1afb
Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
2017-01-26 17:58:57 +0100

    Updating mongoose.config.js

--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -3,10 +3,7 @@ let mongoose = require('mongoose');
 
 module.exports = (config) => {
     var dbURI = config.dev.db;
-    /*if (process.env.NODE_ENV === 'production') {
-        dbURI = process.env.MONGOLAB_URI;
-    }*/
-
+    
     mongoose.connect(dbURI);
 
     // CONNECTION EVENTS
@@ -19,6 +16,4 @@ module.exports = (config) => {
     mongoose.connection.on('disconnected', function() {
         console.log('Mongoose disconnected');
     });
-    // BRING IN YOUR SCHEMAS & MODELS
-    //require('./users');
 };
