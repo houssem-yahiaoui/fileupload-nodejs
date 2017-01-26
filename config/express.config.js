daca1d51e2eb1e9b070b5c2d5010a27da5dc7e6b
Houssem Yahiaoui <houssem.yahiaoui.ai@gmail.com>
2017-01-26 17:58:01 +0100

    updating the express.config.js file

--- config/express.config.js
+++ config/express.config.js
@@ -1,15 +1,9 @@
 "use strict";
 
-let bodyParser = require('body-parser'),
-    logger = require('morgan'),
-    busboyBodyParser = require('busboy-body-parser'),
-    express = require('express'),
-    path = require('path');
+const logger           = require('morgan'),
+      busboyBodyParser = require('busboy-body-parser');
 
 module.exports = (app) => {
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(busboyBodyParser({ limit: '5mb' }));
     app.use(logger('dev'));
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
@@ -17,6 +11,7 @@ module.exports = (app) => {
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         next();
     });
+    app.use(busboyBodyParser({ limit: '10mb' }));  
 
     //[*]Routes Configuration
     let main = require('../routings/routing.js');
