244b8409c801db087ae64f82c2d7a24f371e45d1
hossein <Ali.dragon.swr@gmail.com>
2017-10-06 23:18:53 +0330

    Adding new syntax and fix small issues :simple_smile: :+1:

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   dev: {
     port: process.env.port || 3000,
-    db  : process.env.DB_LINK || "mongodb://127.0.0.1/testgridfs"
+    db  : process.env.DB_LINK || "mongodb://localhost:27017/testgridfs"
   },
   prod: {
     //TODO !
--- config/mongoose.config.js
+++ config/mongoose.config.js
@@ -1,19 +1,19 @@
 "use strict";
 let mongoose = require('mongoose');
 
+// activating Promises for mongoose
+mongoose.Promise = global.Promise;
+
 module.exports = (config) => {
     var dbURI = config.dev.db;
     
-    mongoose.connect(dbURI);
-
-    // CONNECTION EVENTS
-    mongoose.connection.on('connected', function() {
-        console.log('Mongoose connected to ' + dbURI);
-    });
-    mongoose.connection.on('error', function(err) {
-        console.log('Mongoose connection error: ' + err);
-    });
-    mongoose.connection.on('disconnected', function() {
-        console.log('Mongoose disconnected');
+    // using new syntax for mongoose library
+    // check the mongoose doc for more info
+    mongoose.connect(dbURI, {
+    	useMongoClient: true,
+    }).then(() => {
+      console.log(`Connected to ${dbURI}`);
+    }).catch((e) => {
+      throw e;
     });
 };
